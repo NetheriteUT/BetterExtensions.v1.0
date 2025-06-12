fad7635b84825493972d745fb9fd879f4626eae9
Netherite UT <upikabot2.0@gmail.com>
2025-06-12 18:57:13 -0400

    Update background.js

--- Touch Grass/background.js
+++ Touch Grass/background.js
@@ -1,8 +1,16 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.status === 'complete') {
-    chrome.scripting.executeScript({
-      target: { tabId },
-      files: ["content.js"]
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create('touchGrassReminder', { periodInMinutes: 5 });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'touchGrassReminder') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          files: ['content.js']
+        });
+      }
     });
   }
 });
